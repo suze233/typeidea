9270a8d5cede603f460607e56b4ff7399ededdfa
suze <kunliang.me@outlook.com>
2021-08-31 21:00:44 +0800

    update admin

--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Comment
 # Register your models here.
+from typeidea.custom_site import custom_site
+from typeidea.base_admin import BaseOwnerAdmin
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+@admin.register(Comment, site=custom_site)
+class CommentAdmin(BaseOwnerAdmin):
     list_display = ('target', 'nickname', 'content', 'website', 'created_time')
